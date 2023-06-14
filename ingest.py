27ea22285255179259b19d6471e529ad591d8bff
Philip Montgomery <pmont2@illinois.edu>
2023-06-14 16:42:23 -0500

    Made it so only new files will create embeddings

--- ingest.py
+++ ingest.py
@@ -9,7 +9,7 @@ from langchain.vectorstores import Chroma
 from multiprocessing import Pool
 import glob
 from constants import (CHROMA_SETTINGS, DOCUMENT_MAP, PERSIST_DIRECTORY,
-                       SOURCE_DIRECTORY)
+                       SOURCE_DIRECTORY, ROOT_DIRECTORY)
 
 
 def load_single_document(file_path: str) -> List[Document]:
@@ -23,16 +23,17 @@ def load_single_document(file_path: str) -> List[Document]:
     return loader.load()
 
 
-def load_documents(source_dir: str) -> List[Document]:
+def load_documents(source_dir: str, ignored_files: List[str] = []) -> List[Document]:
     # Loads all documents from the source documents directory
     all_files = []
     for ext in DOCUMENT_MAP:
         all_files.extend(
             glob.glob(os.path.join(source_dir, f"**/*{ext}"), recursive=True)
         )
+    filtered_files = [file_path for file_path in all_files if file_path not in ignored_files]
     p = Pool()
     docs = []
-    for file in p.imap_unordered(load_single_document, all_files):
+    for file in p.imap_unordered(load_single_document, filtered_files):
         docs.extend(file)
         print(file)
     p.close()
@@ -69,36 +70,53 @@ def load_documents(source_dir: str) -> List[Document]:
     help="Device to run on. (Default is cpu)",
 )
 def main(device_type):
-    # Load documents and split in chunks
-    print(f"Loading documents from {SOURCE_DIRECTORY}")
-    documents = load_documents(SOURCE_DIRECTORY)
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=50)
-    texts = text_splitter.split_documents(documents)
-    print(f"Loaded {len(documents)} documents from {SOURCE_DIRECTORY}")
-    print(f"Split into {len(texts)} chunks of text")
+    
 
+    #Set topic for topic specific embedding of documents
     topic = input("\nEnter a one word topic that describes your documents (IE: Science, Philosophy, etc): ")
     if not topic:
-        topic = "Science"
+        topic = "STS"
 
    
-    print("\nThis may take a while you will see the model is ready when embedding is finished")
-    # Create embeddings
+    
+    #Set up embedding settings
     embeddings = HuggingFaceInstructEmbeddings(
-        model_name="hkunlp/instructor-large",
+        model_name="hkunlp/instructor-base",
         model_kwargs={"device": device_type},
         query_instruction = "Represent the " + topic + " document",
     )
-    db = Chroma.from_documents(
-        texts,
-        embeddings,
-        persist_directory=PERSIST_DIRECTORY,
-        client_settings=CHROMA_SETTINGS,
-    )
+
+    if(os.path.exists(f"{ROOT_DIRECTORY}/DB")) :
+        # Load documents and split in chunks
+        db = Chroma(persist_directory=PERSIST_DIRECTORY, embedding_function=embeddings, client_settings=CHROMA_SETTINGS)
+        collection = db.get()
+        print(f"Loading documents from {SOURCE_DIRECTORY}")
+        documents = load_documents(SOURCE_DIRECTORY, [metadata['source'] for metadata in collection['metadatas']])
+        text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=50)
+        texts = text_splitter.split_documents(documents)
+        print(f"Loaded {len(documents)} documents from {SOURCE_DIRECTORY}")
+        print(f"Split into {len(texts)} chunks of text")
+        print("\nThis may take a while you will see the model is ready when embedding is finished")
+        # Create embeddings
+        db.add_documents(texts)
+    else :
+        # Load documents and split in chunks
+        print(f"Loading documents from {SOURCE_DIRECTORY}")
+        documents = load_documents(SOURCE_DIRECTORY)
+        text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=50)
+        texts = text_splitter.split_documents(documents)
+        print(f"Loaded {len(documents)} documents from {SOURCE_DIRECTORY}")
+        print(f"Split into {len(texts)} chunks of text")
+        print("\nThis may take a while you will see the model is ready when embedding is finished")
+        # Create embeddings
+        db = Chroma.from_documents(
+            texts,
+            embeddings,
+            persist_directory=PERSIST_DIRECTORY,
+            client_settings=CHROMA_SETTINGS,
+        )
     db.persist()
     db = None
     print("\nThe model is ready to be used run the command 'python runmodel.py'")
-
-
 if __name__ == "__main__":
     main()
