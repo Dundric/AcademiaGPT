6cfc9de890073b65eb6bb2d27b58e499785ce650
Philip M <philipm614@yahoo.com>
2023-08-10 13:19:20 -0500

    Add files via upload

--- ingest.py
+++ ingest.py
@@ -1,15 +1,12 @@
 import os
 from typing import List
-
-import click
 from langchain.docstore.document import Document
 from langchain.embeddings import HuggingFaceInstructEmbeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.vectorstores import Chroma
 from multiprocessing import Pool
 import glob
-from constants import (CHROMA_SETTINGS, DOCUMENT_MAP, PERSIST_DIRECTORY,
-                       SOURCE_DIRECTORY, ROOT_DIRECTORY)
+from constants import (CHROMA_SETTINGS, DOCUMENT_MAP, PERSIST_DIRECTORY, ROOT_DIRECTORY)
 
 
 def load_single_document(file_path: str) -> List[Document]:
@@ -35,50 +32,11 @@ def load_documents(source_dir: str, ignored_files: List[str] = []) -> List[Docum
     docs = []
     for file in p.imap_unordered(load_single_document, filtered_files):
         docs.extend(file)
-        print(file)
     p.close()
     return docs
 
 
-@click.command()
-@click.option(
-    "--device_type",
-    default="cpu",
-    type=click.Choice(
-        [
-            "cpu",
-            "cuda",
-            "ipu",
-            "xpu",
-            "mkldnn",
-            "opengl",
-            "opencl",
-            "ideep",
-            "hip",
-            "ve",
-            "fpga",
-            "ort",
-            "xla",
-            "lazy",
-            "vulkan",
-            "mps",
-            "meta",
-            "hpu",
-            "mtia",
-        ]
-    ),
-    help="Device to run on. (Default is cpu)",
-)
-def main(device_type):
-    
-
-    #Set topic for topic specific embedding of documents
-    topic = input("\nEnter a one word topic that describes your documents (IE: Science, Philosophy, etc): ")
-    if not topic:
-        topic = "STS"
-
-   
-    
+def runall(device_type, topic, directory):
     #Set up embedding settings
     embeddings = HuggingFaceInstructEmbeddings(
         model_name="hkunlp/instructor-base",
@@ -90,22 +48,27 @@ def main(device_type):
         # Load documents and split in chunks
         db = Chroma(persist_directory=PERSIST_DIRECTORY, embedding_function=embeddings, client_settings=CHROMA_SETTINGS)
         collection = db.get()
-        print(f"Loading documents from {SOURCE_DIRECTORY}")
-        documents = load_documents(SOURCE_DIRECTORY, [metadata['source'] for metadata in collection['metadatas']])
-        text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=50)
-        texts = text_splitter.split_documents(documents)
-        print(f"Loaded {len(documents)} documents from {SOURCE_DIRECTORY}")
-        print(f"Split into {len(texts)} chunks of text")
-        print("\nThis may take a while you will see the model is ready when embedding is finished")
-        # Create embeddings
-        db.add_documents(texts)
+        print(f"Loading documents from {directory}")
+        documents = load_documents(directory, [metadata['source'] for metadata in collection['metadatas']])
+        if len(documents) != 0 :
+            for doc in documents:
+                templist = [doc]
+                text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=50)
+                texts = text_splitter.split_documents(templist)
+                print(f"Loaded {len(templist)} documents from {directory}")
+                print(f"Split into {len(texts)} chunks of text")
+                print("\nThis may take a while you will see the model is ready when embedding is finished")
+                # Create embeddings
+                db.add_documents(texts)
+        else :
+            print("All Documents Already Loaded")
     else :
         # Load documents and split in chunks
-        print(f"Loading documents from {SOURCE_DIRECTORY}")
-        documents = load_documents(SOURCE_DIRECTORY)
+        print(f"Loading documents from {directory}")
+        documents = load_documents(directory)
         text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=50)
         texts = text_splitter.split_documents(documents)
-        print(f"Loaded {len(documents)} documents from {SOURCE_DIRECTORY}")
+        print(f"Loaded {len(documents)} documents from {directory}")
         print(f"Split into {len(texts)} chunks of text")
         print("\nThis may take a while you will see the model is ready when embedding is finished")
         # Create embeddings
@@ -118,5 +81,5 @@ def main(device_type):
     db.persist()
     db = None
     print("\nThe model is ready to be used run the command 'python runmodel.py'")
-if __name__ == "__main__":
-    main()
+if __name__ == "__runall__":
+    runall()
